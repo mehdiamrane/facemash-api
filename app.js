dc67441ccc966983c634552663299e4e849614ec
Mehdi Amrane <me.amrane@gmail.com>
2020-03-04 15:36:06 +0100

    Require dotenv on app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
+require('dotenv').config();
 var fileUpload = require('express-fileupload'); // fileupload
-
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
